0ca55196c58b8fe245741c7e2b473c1b131cb1b7
Mikhail Korotin <korotinm@gmail.com>
2026-09-02 21:33:08 +0300

    sprint2: task3 result

--- tasks/task3/gateway/index.js
+++ tasks/task3/gateway/index.js
@@ -1,13 +1,20 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 
 
 const gateway = new ApolloGateway({
   serviceList: [
     { name: 'booking', url: 'http://booking-subgraph:4001' },
     { name: 'hotel', url: 'http://hotel-subgraph:4002' }
-  ]
+  ],
+  buildService: ({url}) => new RemoteGraphQLDataSource({
+    url,
+    willSendRequest({request, context}) {
+      const userid = context.req?.headers?.['userid'];
+      if (userid) request.http.headers.set('userid', userid);
+    }
+  }),
 });
 
 const server = new ApolloServer({ gateway, subscriptions: false });
